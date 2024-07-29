0d8c8816fa05068a095cf1b56d37abbb56024376
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-29 14:19:19 +0530

    Setup Basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <h1>Todo</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
