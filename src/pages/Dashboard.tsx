a27cb1037edc642351be28b4fa93b1a2c65ecaa6
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-30 09:11:38 +0530

    Initail Dsahboard - Input and Button

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,25 @@
-
-import { Button } from "@/components/ui/button"
-import {Input} from '@/components/ui/input'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { Label } from "@/components/ui/label";
 export default function Dashboard() {
   return (
-
-    <div>
-      <Button>Button</Button>
-      <Input/>
+    <div className="w-full h-screen border-2 border-red-400">
+      <div className={"flex justify-around items-center flex-col h-screen"}>
+        <div className="w-[35rem]">
+          <Label htmlFor="todoBox" className={cn('font-bold text-base')}>Enter Task:</Label>
+          <div className="border-2 border-[#343434] flex justify-between items-center rounded-md pr-2 mt-1.5 py-1">
+            <Input
+              id="todoBox"
+              type="text"
+              className={cn("border-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0")}
+              placeholder="Enter a task to get started"
+            />
+            <Button className={cn('w-20 h-8')}>Add</Button>
+          </div>
+        </div>
+        <div>Body</div>
+      </div>
     </div>
-  )
+  );
 }
