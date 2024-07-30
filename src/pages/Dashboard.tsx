00638928b6c49f69bf0ccbaf2404217f843ec70b
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-30 14:34:36 +0530

    Working on DashBoard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,9 +4,33 @@ import { cn } from "@/lib/utils";
 import { Label } from "@/components/ui/label";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import AvatarIMG from "@/assets/avatar.jpeg";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import React from "react";
+
+const ArrowDown = React.forwardRef(() => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9.65 12.65C9.75 12.75 9.86667 12.8 10 12.8C10.1333 12.8 10.25 12.75 10.35 12.65L13.15 9.85C13.3 9.7 13.3373 9.52067 13.262 9.312C13.1873 9.104 13.0333 9 12.8 9H7.2C6.96667 9 6.81267 9.104 6.738 9.312C6.66267 9.52067 6.7 9.7 6.85 9.85L9.65 12.65ZM10 20C8.61667 20 7.31667 19.7373 6.1 19.212C4.88333 18.6873 3.825 17.975 2.925 17.075C2.025 16.175 1.31267 15.1167 0.788 13.9C0.262667 12.6833 0 11.3833 0 10C0 8.61667 0.262667 7.31667 0.788 6.1C1.31267 4.88333 2.025 3.825 2.925 2.925C3.825 2.025 4.88333 1.31233 6.1 0.787C7.31667 0.262333 8.61667 0 10 0C11.3833 0 12.6833 0.262333 13.9 0.787C15.1167 1.31233 16.175 2.025 17.075 2.925C17.975 3.825 18.6873 4.88333 19.212 6.1C19.7373 7.31667 20 8.61667 20 10C20 11.3833 19.7373 12.6833 19.212 13.9C18.6873 15.1167 17.975 16.175 17.075 17.075C16.175 17.975 15.1167 18.6873 13.9 19.212C12.6833 19.7373 11.3833 20 10 20Z"
+      fill="black"
+    />
+  </svg>
+));
+
 export default function Dashboard() {
   return (
-    <div className="container w-full mx-auto border-2">
+    <div className="container w-full mx-auto">
       <div className="absolute right-20 top-10">
         <Avatar className={cn("cursor-pointer")}>
           <AvatarImage src={AvatarIMG} alt="Avatar" />
@@ -14,7 +38,7 @@ export default function Dashboard() {
         </Avatar>
       </div>
       <div
-        className={"flex justify-center gap-16 items-center flex-col h-screen"}
+        className={"flex justify-center items-center flex-col gap-20 min-h-screen"}
       >
         <div className="w-[35rem]">
           <Label htmlFor="todoBox" className={cn("font-bold text-base")}>
@@ -33,26 +57,36 @@ export default function Dashboard() {
           </div>
         </div>
         <div>
-          <div className="bg-white border border-[#343434] p-2 rounded-md w-[19.2rem]">
-            <div className="flex justify-between items-center">
-              <span className="max-w-[30ch] block overflow-hidden text-ellipsis whitespace-nowrap">
-                Make a backup of your current code to prevent any loss of work
-              </span>
-               <div className="cursor-pointer">
-                <svg
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M9.65 12.65C9.75 12.75 9.86667 12.8 10 12.8C10.1333 12.8 10.25 12.75 10.35 12.65L13.15 9.85C13.3 9.7 13.3373 9.52067 13.262 9.312C13.1873 9.104 13.0333 9 12.8 9H7.2C6.96667 9 6.81267 9.104 6.738 9.312C6.66267 9.52067 6.7 9.7 6.85 9.85L9.65 12.65ZM10 20C8.61667 20 7.31667 19.7373 6.1 19.212C4.88333 18.6873 3.825 17.975 2.925 17.075C2.025 16.175 1.31267 15.1167 0.788 13.9C0.262667 12.6833 0 11.3833 0 10C0 8.61667 0.262667 7.31667 0.788 6.1C1.31267 4.88333 2.025 3.825 2.925 2.925C3.825 2.025 4.88333 1.31233 6.1 0.787C7.31667 0.262333 8.61667 0 10 0C11.3833 0 12.6833 0.262333 13.9 0.787C15.1167 1.31233 16.175 2.025 17.075 2.925C17.975 3.825 18.6873 4.88333 19.212 6.1C19.7373 7.31667 20 8.61667 20 10C20 11.3833 19.7373 12.6833 19.212 13.9C18.6873 15.1167 17.975 16.175 17.075 17.075C16.175 17.975 15.1167 18.6873 13.9 19.212C12.6833 19.7373 11.3833 20 10 20Z"
-                    fill="black"
-                  />
-                </svg>
-               </div>
-            </div>
+          <div className="bg-white border border-[#343434] ounded-md p-3 rounded-md">
+            <DropdownMenu>
+              <DropdownMenuTrigger className={cn("outline-none")}>
+                <div className="flex justify-between items-center">
+                  <span className="max-w-[30ch] block overflow-hidden text-ellipsis whitespace-nowrap">
+                    Make a backup of your current code to prevent any loss of
+                    work
+                  </span>
+                  <ArrowDown />
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className={cn("mt-3 shadow-none rounded-none")}>
+                <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                  <Checkbox className={cn("mr-2.5")} />
+                  Lorem ipsum dolor sit amet consectetur.
+                </DropdownMenuItem>
+                <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                  <Checkbox className={cn("mr-2.5")} />
+                  Lorem ipsum dolor sit amet consectetur.
+                </DropdownMenuItem>
+                <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                  <Checkbox className={cn("mr-2.5")} />
+                  Lorem ipsum dolor sit amet consectetur.
+                </DropdownMenuItem>
+                <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                  <Checkbox className={cn("mr-2.5")} />
+                  Lorem ipsum dolor sit amet consectetur.
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
       </div>
