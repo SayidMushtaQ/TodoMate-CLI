95fe80a6bce5c222b33e9e658535a820bcaec659
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-23 18:17:48 +0530

    Add auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import AuthenticatUser from "@/middleware/authenticateUser";
+import AuthenticatUser from "@/middleware/authenticateUser";
 function App() {
   return (
     <BrowserRouter>
-      {/* <AuthenticatUser> */}
+      <AuthenticatUser>
         <Routes>
           <Route index element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
-      {/* </AuthenticatUser> */}
+      </AuthenticatUser>
     </BrowserRouter>
   );
 }
