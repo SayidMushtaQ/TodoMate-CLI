006e71958a0a2371950eb0d91d77e5d7c992fd62
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-26 20:20:32 +0530

    Add User input Todo box

--- src/components/userInputBox.component.tsx
+++ src/components/userInputBox.component.tsx
@@ -1,6 +1,26 @@
-
+import style from "../styles/userInput.module.css";
 export default function UserInputBox() {
   return (
-    <div>UserInputBox</div>
-  )
+    <div className={style.userInputBox}>
+      <div className={style.userInputsSubBox}>
+        <section className={style.userInputsHeading}>
+          <h1>Add your Tasks</h1>
+          <p>
+            Add a task by typing in the input and clicking <b>save.</b>
+          </p>
+        </section>
+        <div className={style.userInputFormBox}>
+          <form action="#">
+            <input
+              type="text"
+              name="todo"
+              id="todo"
+              placeholder="Enter a todo item"
+            />
+            <button>save</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
