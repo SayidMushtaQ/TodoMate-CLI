b179478d8e78e0a5f4e635b185fd2086801407f0
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-26 20:28:49 +0530

    Navigation and Min changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import UserInputBox from './components/userInputBox.component'
 import './App.css'
+import Navigation from './components/Navigation.component'
 function App() {
 
   return (
     <div className='container'>
       <div className='todo-box'>
         <UserInputBox/>
+      </div>
+      <div className='navigation'>
+        <Navigation/>
       </div>
     </div>
   )
---
+++ src/components/Navigation.component.tsx
@@ -0,0 +1,8 @@
+import style from '../styles/navigation.module.css'
+export default function Navigation() {
+  return (
+    <div className={style.navigationBox}>
+      <span>Your tasks</span>
+    </div>
+  )
+}
