3f05742cd0be708cec8002df0b784903afdddb52
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-28 20:34:56 +0530

    Card Drop down

--- src/components/Todo.component.tsx
+++ src/components/Todo.component.tsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import style from "../styles/todos.module.css";
 import { FaCircleArrowDown } from "react-icons/fa6";
-
+import clsx from 'clsx'
 export default function Todo() {
+  const [toggle,setToggle] = useState(false);
   return (
     <div className={style.todo}>
       <span>Add project details</span>
-      <div className={style.subTodos}>
+      <div className={clsx(style.subTodos,{[style.openTodos]:toggle})}>
+        <label>
+          <input type="checkbox" name="option1" value="Option 1" />
+          <span> Lorem, ipsum dolor. </span>
+        </label>
+        <label>
+          <input type="checkbox" name="option1" value="Option 1" />
+          <span> Lorem, ipsum dolor. </span>
+        </label>
+        <label>
+          <input type="checkbox" name="option1" value="Option 1" />
+          <span> Lorem, ipsum dolor. </span>
+        </label>
+        <label>
+          <input type="checkbox" name="option1" value="Option 1" />
+          <span> Lorem, ipsum dolor. </span>
+        </label>
         <label>
           <input type="checkbox" name="option1" value="Option 1" />
           <span> Lorem, ipsum dolor. </span>
         </label>
       </div>
-      <div className={style.todoBottomBlur}>
+      <div onClick={()=>setToggle(!toggle)} className={style.todoBottomBlur}>
         <FaCircleArrowDown size={22} color="4c4a4a"/>
       </div>
     </div>
--- src/components/Todos.component.tsx
+++ src/components/Todos.component.tsx
@@ -9,6 +9,8 @@ export default function Todos() {
       <Todo/>
       <Todo/>
       <Todo/>
+      <Todo/>
+
     </div>
   )
 }
