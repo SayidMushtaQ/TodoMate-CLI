288301042172755c0bea481112f60b7be1ac500e
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-09-01 18:29:56 +0530

    New Components - Login and register

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
 import {Routes,Route} from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 function App() {
   return (
     <Routes>
       <Route index element={ <Dashboard/>}/>
+      <Route path='login' element={ <Login/>}/>
+      <Route path='register' element={ <Register/>}/>
     </Routes>
   )
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,6 @@
+import style from '../styles/login.modules.css'
+export default function Login() {
+  return (
+    <div>Login</div>
+  )
+}
