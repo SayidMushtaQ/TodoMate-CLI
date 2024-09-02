ce594da02624c93bce9b3497f16559ccc54c4b16
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-09-02 20:47:16 +0530

    Add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000/api/v1',
+        changeOrigin: true,
+        secure: false, 
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
