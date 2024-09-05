52ed6de222b12542f6435fe5ec9dcce466cbbbf2
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-09-05 20:19:15 +0530

    GET Todos - API

--- src/components/Todos.component.tsx
+++ src/components/Todos.component.tsx
@@ -1,6 +1,13 @@
 import Todo from './Todo.component'
 import style from '../styles/todos.module.css'
+import {useQuery} from '@tanstack/react-query'
+import { getAllTodosHandler } from '../helpers/todoApi.helper'
 export default function Todos() {
+  const {data:todos} = useQuery({
+    queryKey:['todos'],
+    queryFn:getAllTodosHandler,
+  })
+  console.log(todos)
   return (
     <div className={style.todos}>
       <Todo/>
