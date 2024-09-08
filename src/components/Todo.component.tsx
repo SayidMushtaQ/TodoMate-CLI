bb9b74744cc96387f1906fa6e9b38ae118dac9a6
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-09-08 19:53:44 +0530

    Done - For now

--- src/components/Todo.component.tsx
+++ src/components/Todo.component.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
 import style from "../styles/todos.module.css";
 import { FaCircleArrowDown } from "react-icons/fa6";
@@ -9,27 +10,26 @@ import { useMutation } from "@tanstack/react-query";
 import { getAllSubTodosHandler } from "../helpers/todoApi.helper";
 export default function Todo({ todo }: { todo: TodosRes }) {
   const [toggle, setToggle] = useState(false);
-  const [addToglle, setAddToggle] = useState(false);
-  const { mutate, data: subTodos } = useMutation({
-    /*Better to use useQuery */ mutationFn: getAllSubTodosHandler,
-  });
-  useEffect(() => {
-    const todoID = todo._id;
-    mutate(todoID);
-  }, [todo._id, mutate]);
+  const [addToglle,setAddToggle] = useState(false);
+  const {mutate,data:subTodos} = useMutation({ /*Better to use useQuery */
+    mutationFn:getAllSubTodosHandler
+  })
+  useEffect(()=>{
+      const todoID = todo._id
+      mutate(todoID)
+  },[todo._id,mutate]);
   return (
     <div className={style.todo}>
       <span>{todo.title}</span>
-      {subTodos && (
-        <>
-          <div className={clsx(style.subTodos, { [style.openTodos]: toggle })}>
+      {subTodos && subTodos.map((item:unknown,index:any)=>(
+        <div key={index}>
+              <div className={clsx(style.subTodos, { [style.openTodos]: toggle })}>
             <label>
               <input type="checkbox" name="option1" value="Option 1" />
               <span> Lorem, ipsum dolor. </span>
             </label>
           </div>
           <div
-            onClick={() => setToggle(!toggle)}
             className={clsx(style.todoBottomDefault, {
               [style.todoBottomOnBlur]: !toggle,
             })}
@@ -37,26 +37,24 @@ export default function Todo({ todo }: { todo: TodosRes }) {
             <FaCircleArrowDown
               size={22}
               color="4c4a4a"
+              onClick={() => setToggle(!toggle)}
               className={clsx(style.bottomBlurAction, {
                 [style.blurActionToggle]: toggle,
               })}
               aria-label="Card drop down"
             />
           </div>
-        </>
-      )}
-      <div className={style.addingSubTodo}>
-        <button
-          aria-label="Adding"
-          title="Add sub Todos"
-          onClick={() => setAddToggle(true)}
-        >
-          <IoMdAddCircleOutline size={23} />
-        </button>
-      </div>
-      {addToglle && (
-        <AddingSubTodo setAddToggle={setAddToggle} todoID={todo._id} />
-      )}
+        </div>
+      ))}
+    
+        {toggle && 
+        <div className={style.addingSubTodo}>
+          <button aria-label="Adding" title="Add sub Todos" onClick={()=>setAddToggle(true)}>
+            <IoMdAddCircleOutline size={23}/>
+          </button>
+        </div>
+        }
+      {addToglle && <AddingSubTodo setAddToggle={setAddToggle} todoID={todo._id}/>}
     </div>
   );
 }
